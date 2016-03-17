b1782bfbf07c06adeb1d073d9a86dc0deee491bd
pixie-grasper <himajinn13sei@gmail.com>
2016-03-17 11:39:45 +0900

    fix examples

--- examples/example.cc
+++ examples/example.cc
@@ -1,12 +1,9 @@
 #include <stdio.h>
-#include <stdint.h>
-#include <sys/stat.h>
 #include <prediction-by-partial-matching.h>
 #include <file-vector-converter.h>
-#include <vector>
 
 int main() {
-  auto buffer = ResearchLibrary::File::load("../cpplint,py");
+  auto buffer = ResearchLibrary::File::load("../cpplint.py");
   auto ppm = ResearchLibrary::PredictionByPartialMatching::NumericEncode<MethodC, 4>(buffer);
   printf("%zu -> %zu\n", buffer.size(), ppm.first.size());
   return 0;
